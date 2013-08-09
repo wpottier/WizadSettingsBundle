a8f55eedbc5ca58be91dae7d65338b26081777fc
William POTTIER <wpottier@clever-age.com>
2013-08-09 14:51:05 +0200

    code formatting

--- Form/SettingsType.php
+++ Form/SettingsType.php
@@ -15,13 +15,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class SettingsType extends AbstractType {
+class SettingsType extends AbstractType
+{
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $schema = $options['schema'];
 
-        foreach($schema as $name => $parameter) {
+        foreach ($schema as $name => $parameter) {
             $builder->add(sprintf('setting_%s', $name), $parameter['form']['type'], array('label' => $parameter['name'], 'required' => false));
         }
     }
@@ -29,7 +30,7 @@ class SettingsType extends AbstractType {
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class'      => 'Wizad\SettingsBundle\Model\Settings'
+            'data_class' => 'Wizad\SettingsBundle\Model\Settings'
         ));
 
         $resolver->setRequired(array('schema'));
